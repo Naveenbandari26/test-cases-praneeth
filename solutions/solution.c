9762d351e0b5e0e101d97ead7e70d0db527477d7
J Praneeth <praneeth.janjanam@gmail.com>
2025-04-02 15:55:34 +0530

    Update solution.c

--- solutions/solution.c
+++ solutions/solution.c
@@ -4,15 +4,7 @@
 #include <limits.h>
 
 int maxProfit(int prices[], int size) {
-    if (size < 2) return 0;
 
-    int p = 0, mini = prices[0];
-    for (int i = 1; i < size; i++) {
-        if (prices[i] == INT_MIN) continue;  // Ignore invalid prices
-        if (prices[i] < mini) mini = prices[i];
-        if (prices[i] - mini > p) p = prices[i] - mini;
     }
-    return p;
-}
 
 //

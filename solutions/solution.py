ad3f00647636656695835d237cb544413f18f2b0
Naveenbandari26 <bandarin29@gmail.com>
2025-04-30 12:17:02 +0530

    python 2

--- solutions/solution.py
+++ solutions/solution.py
@@ -3,6 +3,7 @@
 class Solution:
     def maxProfit(self, prices):
         p=0
+        # initializing the minimum price to the first element
         sell=0
         mini=prices[0]
         for i in range(1,len(prices)):
